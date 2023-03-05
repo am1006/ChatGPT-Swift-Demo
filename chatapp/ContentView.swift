43adce9f7b76bc3fb46c9f8e18d511523a9ca551
am1006 <am1006@users.noreply.github.com>
2023-03-05 13:43:25 +1100

    Use ddiu api

--- chatapp/ContentView.swift
+++ chatapp/ContentView.swift
@@ -6,12 +6,111 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct ChatView: View {
   @State private var message = ""
   @State private var botname = "LeoBot"
   @State private var chatHistory: [ChatMessage] = []
   @State private var isThinking: Bool = false
+
+  private let chatGPTURL: String = "https://chatgpt.ddiu.me/api/generate"
+
+  private func prepareRequest(message: ChatMessage) -> URLRequest {
+
+    let parameters: Parameters = Parameters(
+      messages: [
+        ChatCompletionMessage(role: "user", content: message.content)
+      ])
+
+    var request = URLRequest(url: URL(string: chatGPTURL)!)
+    request.httpMethod = "POST"
+
+    let encoder = JSONEncoder()
+    if let encoded = try? encoder.encode(parameters) {
+      request.httpBody = encoded
+    }
+
+    debugPrint("Sending request to \(chatGPTURL)")
+    debugPrint("Request body: \(String(describing: request.httpBody))")
+
+    return request
+  }
+
+  private func sendMessage() async throws {
+    if message.isEmpty {
+      return
+    }
+
+    var newChatMessage = ChatMessage(role: .user, content: message)
+    chatHistory.append(newChatMessage)
+
+    isThinking.toggle()
+    message = ""
+    
+//      let params = [
+//        "role": "user",
+//        "content": newChatMessage.content
+//      ]
+//
+//      let headers: HTTPHeaders = [
+//          "Accept": "*/*",
+//          "Accept-encoding": "gzip, deflate, br",
+//          "content-type": "text/plain;charset=UTF-8",
+//          "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+//      ]
+//
+//      let streamTask = AF.streamRequest(
+//        chatGPTURL,
+//        method: .post,
+//        parameters: params,
+//        headers: headers,
+//        automaticallyCancelOnStreamError: true).validate().streamTask()
+//
+//      for await data in streamTask.streamingStrings() {
+//          var completionMessage = (data.value != nil) ? data.value : ""
+//          if completionMessage.hasPrefix("\n\n") {
+//               completionMessage.removeFirst(2)
+//             }
+//        debugPrint("After: \(completionMessage)")
+//
+//          let newChatMessage = ChatMessage(role: .assistant, content: completionMessage)
+//          debugPrint("After: \(completionMessage)")
+//          chatHistory.append(newChatMessage)
+//          debugPrint(data.value)
+//
+//          isThinking.toggle()
+//      }
+
+
+    // TODO: Set "system" messages to fine tune the bot
+    //        let staticResponse = ChatMessage(role: .assistant, content: "Cool")
+    //        chatHistory.append(staticResponse)
+    let request = prepareRequest(message: newChatMessage)
+
+    let (data, response) = try await URLSession.shared.data(for: request)
+
+
+    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+      debugPrint("Request Failure Error: ")
+      isThinking.toggle()
+      throw URLError(.cannotParseResponse)
+    }
+
+    var completionMessage = String(decoding: data, as: UTF8.self)
+    debugPrint("Original: \(completionMessage)")
+
+    // if the first two characters are \n\n, remove them
+    if completionMessage.hasPrefix("\n\n") {
+      completionMessage.removeFirst(2)
+    }
+    debugPrint("After: \(completionMessage)")
+
+    newChatMessage = ChatMessage(role: .assistant, content: completionMessage)
+    chatHistory.append(newChatMessage)
+
+    isThinking.toggle()
+  }
 
   var body: some View {
     NavigationView {
@@ -40,7 +139,9 @@ struct ChatView: View {
 
             if !message.isEmpty {
               Button {
-                sendMessage()
+                Task {
+                  try await sendMessage()
+                }
               } label: {
                 Image(systemName: "paperplane")
               }
@@ -64,92 +165,6 @@ struct ChatView: View {
       }
       .navigationTitle(botname)
     }
-
-  }
-
-  private let chatGPTURL = "https://api.openai.com/v1/chat/completions"
-  private let openAIModel = "gpt-3.5-turbo"
-  private let openAIApiKey = ""
-
-  private func prepareRequest(message: ChatMessage) -> URLRequest {
-
-    let parameters: Parameters = Parameters(
-      model: openAIModel,
-      messages: [
-        ChatCompletionMessage(role: "user", content: message.content)
-      ])
-
-    var request = URLRequest(url: URL(string: chatGPTURL)!)
-    request.httpMethod = "POST"
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-    request.addValue("Bearer \(openAIApiKey)", forHTTPHeaderField: "Authorization")
-
-    let encoder = JSONEncoder()
-    if let encoded = try? encoder.encode(parameters) {
-      request.httpBody = encoded
-    }
-
-    print("Sending request to \(chatGPTURL)")
-    print("Request body: \(String(describing: request.httpBody))")
-
-    return request
-  }
-
-  func sendMessage() {
-    if message.isEmpty {
-      return
-    }
-
-    let newChatMessage = ChatMessage(role: .user, content: message)
-    chatHistory.append(newChatMessage)
-
-    isThinking.toggle()
-    message = ""
-
-    // TODO: Set "system" messages to fine tune the bot
-
-    //        let staticResponse = ChatMessage(role: .assistant, content: "Cool")
-    //        chatHistory.append(staticResponse)
-
-    let request = prepareRequest(message: newChatMessage)
-
-    let session = URLSession.shared
-    let task = session.dataTask(with: request) { (data, response, error) in
-      if let err = error {
-          
-        print("Request Failure Error: \(err)")
-        isThinking.toggle()
-          
-      } else if let data = data {
-          
-        do {
-            print(data)
-          let res = try JSONDecoder().decode(ChatCompletionResponse.self, from: data)
-            var completionMessage = res.choices.first!.message.content
-            
-            // remove the first two \n
-            let index = completionMessage.index(completionMessage.startIndex, offsetBy: 2)
-            completionMessage = String(completionMessage[index...])
-            
-          // Print the debugging message
-          print("Chat response: \(res)")
-          print("Completion message: \(completionMessage)")
-
-          DispatchQueue.main.async {
-            let newChatMessage = ChatMessage(role: .assistant, content: completionMessage)
-            chatHistory.append(newChatMessage)
-
-            isThinking.toggle()
-          }
-        } catch {
-          print("Request Success but Decoding Error: \(error)")
-            isThinking.toggle()
-        }
-      }
-    }
-    task.resume()
-
   }
 }
 
@@ -159,72 +174,15 @@ struct ChatView_Previews: PreviewProvider {
   }
 }
 
-// {
-//   "id": "chatcmpl-123",
-//   "object": "chat.completion",
-//   "created": 1677652288,
-//   "choices": [{
-//     "index": 0,
-//     "message": {
-//       "role": "assistant",
-//       "content": "\n\nHello there, how may I assist you today?",
-//     },
-//     "finish_reason": "stop"
-//   }],
-//   "usage": {
-//     "prompt_tokens": 9,
-//     "completion_tokens": 12,
-//     "total_tokens": 21
-//   }
-// }
-
-struct ChatCompletionResponse: Codable {
-    let id: String
-    let object: String
-    let created: Int
-    let choices: [ChatCompletionChoice]
-    let usage: ChatCompletionUsage
-}
-
-struct ChatCompletionChoice: Codable {
-    let index: Int
-    let message: ChatCompletionMessage
-    let finishReason: String
-    
-    enum CodingKeys: String, CodingKey {
-        case index, message
-        case finishReason = "finish_reason"
-    }
+struct Parameters: Codable {
+  let messages: [ChatCompletionMessage]
 }
 
 struct ChatCompletionMessage: Codable {
-    let role: String
-    let content: String
-    
-    enum CodingKeys: String, CodingKey {
-        case role, content
-    }
-}
+  let role: String
+  let content: String
 
-struct ChatCompletionUsage: Codable {
-    let promptTokens: Int
-    let completionTokens: Int
-    let totalTokens: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case promptTokens = "prompt_tokens"
-        case completionTokens = "completion_tokens"
-        case totalTokens = "total_tokens"
-    }
-}
-
-
-// {
-//   "model": "gpt-3.5-turbo",
-//   "messages": [{"role": "user", "content": "Hello!"}]
-// }
-
-struct Parameters: Codable {
-  let model: String
-  let messages: [ChatCompletionMessage]
+  enum CodingKeys: String, CodingKey {
+    case role, content
+  }
 }
